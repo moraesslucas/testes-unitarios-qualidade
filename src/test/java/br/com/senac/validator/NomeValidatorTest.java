02a378b94f0de8af77311a8561f2389107f8c222
Lucas Moraes <lucas.moraes@avenuecode.com>
2019-10-07 19:58:30 -0300

    Implementando testes de falha

--- src/test/java/br/com/senac/validator/NomeValidatorTest.java
+++ src/test/java/br/com/senac/validator/NomeValidatorTest.java
@@ -12,22 +12,36 @@ public class NomeValidatorTest {
     private static final String INVALID_NOME_TAMANHO = "Nome muito grande deve gerar erro ao testar";
     private static final String VALID_NOME = "Carlos Nome";
 
+    public final ExpectedException expectedException = ExpectedException.none();
+
     private static final NomeValidator nomeValidator = new NomeValidator();
 
 //  metodo deve jogar exceção quando nome for nulo
     @Test
     public void validateShouldThrowExceptionWhenNomeIsNull() {
+        expectedException.expect(NomeInvalidoException.class);
+        expectedException.expectMessage(Mensagens.NOME_NULO);
+
+        nomeValidator.validate(null);
     }
 
 //  metodo deve jogar exceção quando nome não for alfanumérico
     @Test
     public void validateShouldThrowExceptionWhenNomeIsNotAlphaNumeric() {
+        expectedException.expect(NomeInvalidoException.class);
+        expectedException.expectMessage(Mensagens.NOME_CARACTER_INVALIDO);
+
+        nomeValidator.validate(INVALID_NOME_ESPECIAL);
     }
 
 
 //  metodo deve jogar exceção quando tamanho do parâmetro for maior que 40
     @Test
     public void validateShouldThrowExceptionWhenNomeSizeIsGreaterThan40() {
+        expectedException.expect(NomeInvalidoException.class);
+        expectedException.expectMessage(Mensagens.NOME_TAMANHO_INVALIDO);
+
+        nomeValidator.validate(INVALID_NOME_TAMANHO);
     }
 
 //  metodo deve ter sucesso quando nome for válido
